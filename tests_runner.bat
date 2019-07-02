3eef3d1d4b77666caa576a57c9b4d2d55a574ce6
simongarisch <gatman946@gmail.com>
2019-07-03 07:27:23 +1000

    Adding python env27 tests to test_runner.bat

--- tests_runner.bat
+++ tests_runner.bat
@@ -2,4 +2,7 @@
 call "./env/Scripts/activate"
 py.test --cov-report html --cov --doctest-modules --cache-clear --ignore=examples
 
+call "./env27/Scripts/activate"
+py.test --cov-report html --cov --doctest-modules --cache-clear --ignore=examples
+
 pause
