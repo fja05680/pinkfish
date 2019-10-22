de6b4ad76130262f7eb0d7ef15d71ecb19b47db7
Farrell Aultman <fja0568@gmail.com>
2019-10-21 20:13:53 -0700

    __init__.py: change format

--- pinkfish/__init__.py
+++ pinkfish/__init__.py
@@ -1,8 +1,41 @@
-from pinkfish.fetch import fetch_timeseries, select_tradeperiod, clear_timeseries
-from pinkfish.trade import TradeLog, TradeState, DailyBal
-from pinkfish.statistics import (stats, currency,
-    summary, summary2, summary3, summary4, summary5)
-from pinkfish.benchmark import Benchmark
-from pinkfish.plot import plot_equity_curve, plot_trades, plot_bar_graph
-from pinkfish.indicator import CROSSOVER
-from pinkfish.utility import print_full, read_config
+from .fetch import (
+    fetch_timeseries,
+    select_tradeperiod,
+    clear_timeseries
+)
+
+from .trade import (
+    TradeLog,
+    TradeState,
+    DailyBal
+)
+
+from .statistics import (
+    stats,
+    currency,
+    summary,
+    summary2,
+    summary3,
+    summary4,
+    summary5
+)
+
+from .benchmark import (
+    Benchmark
+)
+
+from .plot import (
+    plot_equity_curve,
+    plot_trades,
+    plot_bar_graph
+)
+
+from .indicator import (
+    CROSSOVER
+)
+
+from .utility import (
+    print_full,
+    read_config
+)
+
