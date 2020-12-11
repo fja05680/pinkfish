26eb8faee98b713fd92ba1d30842764077457f69
Farrell Aultman <fja0568@gmail.com>
2020-12-11 15:06:21 -0800

    removed test code since it's so out of date

--- tests_runner.bat
+++
@@ -1,8 +0,0 @@
-
-call "./env/Scripts/activate"
-py.test --cov-report html --cov --doctest-modules --cache-clear --ignore=examples
-
-call "./env27/Scripts/activate"
-py.test --cov-report html --cov --doctest-modules --cache-clear --ignore=examples
-
-pause
